db97ab76d893b8490826ea5ba632249131df2b3f
JOSE DE JESUS ROSALES MUNOZ <rosalesjj@globalhitss.com>
2024-12-03 17:17:28 -0800

    desafio 2  y desafio 3 actualizado

--- test/Desafio2_example.test.js
+++ test/Desafio2_example.test.js
@@ -2,49 +2,50 @@ const expect = require('chai').expect
 const { before, after, it, describe } = require('mocha')
 const puppeteer = require('puppeteer')
 
-describe ('Desafio 2 - Evaluar la función click en un elemento de una página web mediante nombre, XPath y CSS', () => {
+describe ('Desafio 2 - Evaluar la función click con el  nombre, XPath y CSS', () => {
     let browser, page
 
     before(async () => {
         browser = await puppeteer.launch({ headless: false })  
         page = await browser.newPage()
+        await page.setViewport({ width: 1920, height: 1080 })
     })
     after(async () => {
         await browser.close()
     })
 
     beforeEach(async () => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
+        await page.goto('https://www.costco.com.mx/')
     })
 
     afterEach(async () => {
         await waitForTimeout(3000)
     })
 
-    it('Debe abrir el navegador y evaluar click validando que aparezca un elemento usando el texto del botón', async () => {
-        await page.waitForSelector('#searchTerm')
-        //await page.click('#onlineBankingMenu > div > strong');
-        await page.$$eval('div > strong', buttons => {
+    it('Buscar en el navegador el elemento HTML "a" y compararlo  el texto LLantas', async () => {
+        await page.waitForSelector('#siteLogoContainer')  
+      
+        await page.$$eval('body > main > sip-header > header > div.container-fluid > div:nth-child(2) > nav > div > div.navigation-wrapper.v-align-container > div > div.NavigationBar.has-components > ul:nth-child(3) > li > a', buttons => {
             for (const button of buttons) {
-              if (button.textContent === 'Online Banking') {
+              if (button.textContent === 'Llantas') {  
                 button.click();
-                break; // Clicking the first matching button and exiting the loop
+                break; 
               }
             }
         });
-        await page.waitForSelector('body > div.wrapper > div.container > div > div:nth-child(2) > div > div > div > div > div > h1');
+        await page.waitForSelector('body > main > div.container.bottom-header.BottomHeader.has-components > sip-cms-breadcrumb > div > ol > li:nth-child(2) > span');
     })
 
     it('abrir el navegador y evaluar click con el xPath del botón', async () => {
-        await page.waitForSelector('#searchTerm')
-        await page.click('xpath///*[@id="onlineBankingMenu"]/div/strong');
-        await page.waitForSelector('body > div.wrapper > div.container > div > div:nth-child(2) > div > div > div > div > div > h1');
+        await page.waitForSelector('#siteLogoContainer')
+        await page.click('xpath///html/body/main/sip-header/header/div[2]/div[2]/nav/div/div[1]/div/div[2]/ul[3]/li/a');
+        await page.waitForSelector('body > main > div.container.bottom-header.BottomHeader.has-components > sip-cms-breadcrumb > div > ol > li:nth-child(2) > span');
     })
 
-    it('Debe abrir el navegador y evaluar click con  id del botón', async () => {
-        await page.waitForSelector('#searchTerm')
-        await page.click("#onlineBankingMenu");
-        await page.waitForSelector('body > div.wrapper > div.container > div > div:nth-child(2) > div > div > div > div > div > h1');
+    it('Abrir el navegador y evaluar click mediante CSS', async () => {
+        await page.waitForSelector('#siteLogoContainer')
+        await page.click("body > main > sip-header > header > div.container-fluid > div:nth-child(2) > nav > div > div.navigation-wrapper.v-align-container > div > div.NavigationBar.has-components > ul:nth-child(3) > li > a");
+        await page.waitForSelector('body > main > div.container.bottom-header.BottomHeader.has-components > sip-cms-breadcrumb > div > ol > li:nth-child(2) > span');
     })
 });
 
--- test/Desafio3_example.test.js
+++ test/Desafio3_example.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect
 const { before, after, it, describe } = require('mocha')
 const puppeteer = require('puppeteer')
 
-describe ('Desafio 3 - Simular una compra evaluando que el precio en el carrito sea igual al precio del producto', () => {
+describe ('Desafio 3 - Verificar que el precio de un producto en la pagina principal sea igual que en el carrito', () => {
     let browser, page
 
     before(async () => {
@@ -14,28 +14,28 @@ describe ('Desafio 3 - Simular una compra evaluando que el precio en el carrito 
     })
 
     beforeEach(async () => {
-        await page.goto('https://www.mercadolibre.com.mx/')
+        await page.goto('https://www.costco.com.mx/')
     })
 
     afterEach(async () => {
         await waitForTimeout(5000) 
     })
 
-    it('Debe abrir el navegador y evaluar click validando que aparezca un elemento usando el texto del botón', async () => {
-        await page.waitForSelector('.dynamic-carousel__item-container');
-        const price = await page.$eval("div > a > div > div > div.dynamic-carousel__price-block > span > span", element => element.textContent);
-        console.log('Price: ', price.replace('$', '').replace(' ', ''));
+    it('Seleccionar el producto Bluey Inflable Decorativo 2.4m ', async () => {
+        await page.waitForSelector('#siteLogoContainer');
+        const priceOriginal = await page.$eval("body > main > div.page-content.container.main-wrapper > sip-home-page > div > div.no-space > sip-page-slot.Section5.has-components > sip-product-carousel:nth-child(1) > sip-product-carousel-base > div > div.carousel > sip-carousel > owl-carousel-o > div > div.owl-stage-outer.ng-star-inserted > owl-stage > div > div > div:nth-child(1) > div > sip-product-carousel-item > div > div.item-price.ng-star-inserted > sip-product-price-panel > div > div.original-price.ng-star-inserted > span > sip-format-price > span", element => element.textContent);
+        console.log('Precio inical  del Bluey Inflable Decorativo:: ', priceOriginal.replace('$', '').replace(' ', ''));
 
-        await page.click('div > a > div > div > div.dynamic-carousel__price-block > span > span');
-        await page.waitForSelector('#price > div > div.ui-pdp-price__main-container > div.ui-pdp-price__second-line > span:nth-child(1) > span > span.andes-money-amount__fraction');
-        const priceProduct = await page.$eval('#price > div > div.ui-pdp-price__main-container > div.ui-pdp-price__second-line > span:nth-child(1) > span > span.andes-money-amount__fraction', element => element.textContent);
-        console.log('Price Product: ', priceProduct.replace('$', '').replace(' ', ''));
-        expect(price.replace('$', '').replace(' ', '')).to.be.equals(priceProduct.replace('$', '').replace(' ', ''));
+        await page.click('body > main > div.page-content.container.main-wrapper > sip-home-page > div > div.no-space > sip-page-slot.Section5.has-components > sip-product-carousel:nth-child(1) > sip-product-carousel-base > div > div.carousel > sip-carousel > owl-carousel-o > div > div.owl-stage-outer.ng-star-inserted > owl-stage > div > div > div:nth-child(1) > div > sip-product-carousel-item > div > span > div > a');
+        await page.waitForSelector('body > main > div.page-content.container.main-wrapper > sip-product-details-page > sip-product-details > div > sip-product-title > div > h1');
+        const precioFinal = await page.$eval('body > main > div.page-content.container.main-wrapper > sip-product-details-page > sip-product-details > div > div.header-content-container.col-xs-12.col-sm-12.col-md-6.col-tab-6.ng-star-inserted > div > div:nth-child(1) > div > div > div.product-price.ng-star-inserted > div > sip-product-price-panel > div > div.price-original.ng-star-inserted > span > sip-format-price > span', element => element.textContent);
+        console.log('Precio final del Bluey Inflable Decorativo: ', precioFinal.replace('$', '').replace(' ', ''));
+        expect(priceOriginal.replace('$', '').replace(' ', '')).to.be.equals(precioFinal.replace('$', '').replace(' ', ''));
     })
 });
 
 function waitForTimeout(time) {
-	return new Promise(function (resolve) {
+	return new Promise(function (resolve) { 
 		setTimeout(resolve, time)
 	})
 }
