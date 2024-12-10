e6bdf5978376778d4cdd75dd22ec0bac3747d5bd
JOSE DE JESUS ROSALES MUNOZ <rosalesjj@globalhitss.com>
2024-12-09 20:16:41 -0800

    Se modifico para seleccionar el caarito y despues comparar el precio

--- test/Desafio3_example.test.js
+++ test/Desafio3_example.test.js
@@ -20,11 +20,11 @@ describe ('Desafio 3 - Verificar que el precio de un producto en la pagina princ
     afterEach(async () => {
         await waitForTimeout(5000) 
     })
-
+/*
     it('Seleccionar el producto Bluey Inflable Decorativo 2.4m ', async () => {
         await page.waitForSelector('#siteLogoContainer');
         const priceOriginal = await page.$eval("body > main > div.page-content.container.main-wrapper > sip-home-page > div > div.no-space > sip-page-slot.Section5.has-components > sip-product-carousel:nth-child(1) > sip-product-carousel-base > div > div.carousel > sip-carousel > owl-carousel-o > div > div.owl-stage-outer.ng-star-inserted > owl-stage > div > div > div:nth-child(1) > div > sip-product-carousel-item > div > div.item-price.ng-star-inserted > sip-product-price-panel > div > div.original-price.ng-star-inserted > span > sip-format-price > span", element => element.textContent);
-        console.log('Precio inical  del Bluey Inflable Decorativo:: ', priceOriginal.replace('$', '').replace(' ', ''));
+        console.log('Precio inical  del Bluey Inflable Decorativo: ', priceOriginal.replace('$', '').replace(' ', ''));
 
         await page.click('body > main > div.page-content.container.main-wrapper > sip-home-page > div > div.no-space > sip-page-slot.Section5.has-components > sip-product-carousel:nth-child(1) > sip-product-carousel-base > div > div.carousel > sip-carousel > owl-carousel-o > div > div.owl-stage-outer.ng-star-inserted > owl-stage > div > div > div:nth-child(1) > div > sip-product-carousel-item > div > span > div > a');
         await page.waitForSelector('body > main > div.page-content.container.main-wrapper > sip-product-details-page > sip-product-details > div > sip-product-title > div > h1');
@@ -32,6 +32,27 @@ describe ('Desafio 3 - Verificar que el precio de un producto en la pagina princ
         console.log('Precio final del Bluey Inflable Decorativo: ', precioFinal.replace('$', '').replace(' ', ''));
         expect(priceOriginal.replace('$', '').replace(' ', '')).to.be.equals(precioFinal.replace('$', '').replace(' ', ''));
     })
+});
+*/
+it('Seleccionar el producto ', async () => {
+    await page.waitForSelector('#siteLogoContainer');
+    const priceOriginal = await page.$eval("body > main > div.page-content.container.main-wrapper > sip-home-page > div > div.no-space > sip-page-slot.Section5.has-components > sip-product-carousel:nth-child(1) > sip-product-carousel-base > div > div.carousel > sip-carousel > owl-carousel-o > div > div.owl-stage-outer.ng-star-inserted > owl-stage > div > div > div:nth-child(1) > div > sip-product-carousel-item > div > div.item-price.ng-star-inserted > sip-product-price-panel > div > div.original-price.ng-star-inserted > span > sip-format-price > span", element => element.textContent);
+    console.log('Precio inical producto: ', priceOriginal.replace('$', '').replace(' ', ''));
+    await page.click('body > main > div.page-content.container.main-wrapper > sip-home-page > div > div.no-space > sip-page-slot.Section5.has-components > sip-product-carousel:nth-child(1) > sip-product-carousel-base > div > div.carousel > sip-carousel > owl-carousel-o > div > div.owl-stage-outer.ng-star-inserted > owl-stage > div > div > div:nth-child(1) > div > sip-product-carousel-item > div > span > div > a');
+    await page.waitForSelector('#add-to-cart-button', element => element.textContent);
+    console.log('Producto seleccionado');
+  
+       await page.click('#add-to-cart-button'); 
+
+       console.log('Esperar el producto en el carrito');
+       await page.waitForSelector('#addToCartModal > div.modal-header > span', element => element.textContent);
+       console.log('Producto mostrado en el carrito');
+    // const precioFinal = await page.$eval('document.querySelector("#addToCartModal > div.modal-body-wrapper > div.modal-body.main-product-body.js-main-product-body > div > sip-add-to-cart-entry > div > div > div.details > div:nth-child(4) > sip-cart-item-price > div > sip-format-price > span")', element => element.textContent);
+     //const precioFinal = await page.$eval('#addToCartModal > div.modal-body-wrapper > div.modal-body.main-product-body.js-main-product-body > div > sip-add-to-cart-entry > div > div > div.details > div:nth-child(4) > sip-cart-item-price > div > sip-format-price > span', element => element.textContent);
+     const precioFinal = await page.$eval('body > main > div.page-content.container.main-wrapper > sip-product-details-page > sip-product-details > div > div.header-content-container.col-xs-12.col-sm-12.col-md-6.col-tab-6.ng-star-inserted > div > div:nth-child(1) > div > div > div.product-price.ng-star-inserted > div > sip-product-price-panel > div > div.price-original.ng-star-inserted > span > sip-format-price > span', element => element.textContent);
+    console.log('Precio final del producto: ', precioFinal.replace('$', '').replace(' ', ''));
+    expect(priceOriginal.replace('$', '').replace(' ', '')).to.be.equals(precioFinal.replace('$', '').replace(' ', ''));
+})
 });
 
 function waitForTimeout(time) {
